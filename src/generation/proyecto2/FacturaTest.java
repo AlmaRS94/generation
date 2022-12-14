910f1ee6caeee35465efd0eb7ba0b14cd6357ed4
Alma Rosales Sanchez <almarosalessanchez94@gmail.com>
2022-12-13 23:38:30 -0600

    Proyecto 2 terminado.

---
+++ src/generation/proyecto2/FacturaTest.java
@@ -0,0 +1,11 @@
+package generation.proyecto2;
+
+public class FacturaTest{ 
+
+	public static void main(String[] args) {
+		Factura fact1 = new Factura("FACT1","Compra de teclados",3 ,2.5);
+		System.out.println(fact1.getTotalFactura());
+		
+	} 
+
+}
